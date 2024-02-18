30a0dc21a9b455a1e2eae7b6cde12fa1060e15ab
Nikita Boyarkin <nikitaboyarkin15@gmail.com>
2024-02-18 17:40:29 +0300

    files added

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-const http = require('node:http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const Agent = require('./agent'); // Импорт агента
+const VERSION = 7; // Версия сервера
+let teamName = "teamA"; // ИМЯ команды
+let agent = new Agent(); // Создание экземпляра агента
+require('./socket')(agent, teamName, VERSION) //Настройка сокета
+agent.socketSend("move", `-15 0`) // Размещение игрока на поле
